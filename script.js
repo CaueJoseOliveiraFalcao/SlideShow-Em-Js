395c120ce2facad1040605082dd5960b27c5ad5f
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2022-04-25 11:55:53 -0300

    final

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
+
 'use strict';
 
 const images = [
-    {'id' : '1' , 'url' : '.img/chrono.jpg'},
-    {'id' : '2' , 'url' : '.img/inuyasha.jpg'},
-    {'id' : '3' , 'url' : '.img/tenchi.jpg'},
-    {'id' : '4' , 'url' : '.img/tenjhotenge.jpg'},
-    {'id' : '5' , 'url' : '.img/yuyuhakuhsho.jpg'},
-    {'id' : '6' , 'url' : '.img/ippo.jpg'}
+    { 'id': '1', 'url':'./img/chrono.jpg' },
+    { 'id': '2', 'url':'./img/inuyasha.jpg' },
+    { 'id': '3', 'url':'./img/tenchi.jpg' },
+    { 'id': '4', 'url':'./img/tenjhotenge.jpg' },
+    { 'id': '5', 'url':'./img/yuyuhakusho.jpg' },
+    { 'id': '6', 'url':'./img/ippo.png' },
 ]
 
-const container = document.querySelector('.container-items')
+const containerItems = document.querySelector('#container-items');
 
-const loadImagens = (images , container) => {
-    images.foreach (image => {
+const loadImages = ( images, container ) => {
+    images.forEach ( image => {
         container.innerHTML += `
-        <div class='item'>
-            <img src='${Image.url}'>
-        </div>`
+            <div class='item'>
+                <img src='${image.url}'
+            </div>
+        `
     })
 }
 
-loadImagens ( images , container );
+loadImages( images, containerItems );
+
+let items = document.querySelectorAll('.item');
+
+const previous = () => {
+    containerItems.appendChild(items[0]);
+    items = document.querySelectorAll('.item');
+}
+
+const next = () => {
+    const LastItem = items[items.length - 1]
+    containerItems.insertBefore( LastItem , items[0] );
+    items = document.querySelectorAll('.item');
+}
+
+document.querySelector('#previous').addEventListener('click' , previous);
+document.querySelector('#next').addEventListener('click' , next);
---
+++ testes.js
@@ -0,0 +1,6 @@
+const Padaria = (tipo_de_pao,quantidade) => {
+    return console.log(tipo_de_pao + quantidade)
+}
+
+
+Padaria( 'pao' , 2)
